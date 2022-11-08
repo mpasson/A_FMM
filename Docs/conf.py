0321878dd3caa1271eb078dc72129b307fbe3b35
Marco Passoni <mpasson91@gmail.com>
2022-11-08 15:35:24 +0100

    Blacked conf.py

--- Docs/conf.py
+++ Docs/conf.py
@@ -12,15 +12,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../.'))
-sys.path.insert(0, os.path.abspath('../../.'))
+
+sys.path.insert(0, os.path.abspath("../."))
+sys.path.insert(0, os.path.abspath("../../."))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'A_FMM Documentation'
-copyright = '2020, Marco Passoni'
-author = 'Marco Passoni'
+project = "A_FMM Documentation"
+copyright = "2020, Marco Passoni"
+author = "Marco Passoni"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,26 +30,26 @@ author = 'Marco Passoni'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
     "nbsphinx",
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.intersphinx",
 ]
 
 ### AutoSummary settings
 
-#autoclass_content = 'both'
+# autoclass_content = 'both'
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 autosummary_generate = True
 
@@ -57,11 +58,13 @@ autosummary_generate = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'classic'
+# html_theme = 'classic'
 html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+
 def setup(app):
-    app.add_css_file('theme_overrides.css') 
+    app.add_css_file("theme_overrides.css")
