9d0f5d88ef963b261f3bc36d6e9fd14e001488d9
Marco Passoni <mpasson91@gmail.com>
2023-08-03 21:46:55 +0200

    moved pyproject.toml to hatch and related changes.

--- A_FMM/__init__.py
+++ A_FMM/__init__.py
@@ -3,10 +3,22 @@ A fully vectorial method for solving Maxwell equations that combines a Fourier-b
 This approach is well suited to calculate modes, transmission, reflection, scattering and absorption of multi-layered structures.
 Moreover, support for Bloch modes of periodic structures allows for the simulation of photonic crystals or waveguide Bragg gratings.
 """
+import os
+
 
 from A_FMM.layer import *
 from A_FMM.scattering import S_matrix
 from A_FMM.creator import Creator
 from A_FMM.stack import Stack
 import A_FMM.inputs as inputs
-from A_FMM._version import __version__
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    from hatch_vcs.version_source import VCSVersionSource
+    import toml
+
+    basedir = os.path.split(os.path.dirname(__file__))[0]
+    config = toml.load(os.path.join(basedir, "pyproject.toml"))
+    vcs_version = VCSVersionSource(basedir, config["tool"]["hatch"]["version"])
+    __version__ = vcs_version.get_version_data()["version"]
--- A_FMM/_version.py
+++
@@ -1,7 +0,0 @@
-import os
-import setuptools_git_versioning
-
-__version__ = setuptools_git_versioning.get_version(
-    root=os.path.join(*os.path.split(os.path.dirname(__file__))[:-1])
-)
-__version__ = str(__version__)
